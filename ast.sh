f2d20ee4ae019a1b90f8349d03e29952fb448f4c
Ubuntu <mdfeist@ualberta.ca>
2016-03-18 15:22:59 -0700

    Add eviroment argument to ast.sh.

--- ast.sh
+++ ast.sh
@@ -2,11 +2,13 @@
 
 if [ ${2: -5} == ".java" ] || [ ${3: -5} == ".java" ]
 then
+	echo "#STATS_START"
     echo "#FILE1 | $2"
     echo "#FILE2 | $3"
-	echo "#STATS_START"
 
-	timeout 10s java -cp ~/Documents/UofA/CMPUT664/MSR-Challenge-2016/gumtree-spoon-ast-diff/target/gumtree-spoon-ast-diff-0.0.3-SNAPSHOT-jar-with-dependencies.jar fr.inria.sacha.spoon.diffSpoon.DiffSpoonImpl $1 $2 $3
+    echo $CMPUT664_PROJECT
+
+	timeout 10s java -cp $CMPUT664_PROJECT/gumtree-spoon-ast-diff/target/gumtree-spoon-ast-diff-0.0.3-SNAPSHOT-jar-with-dependencies.jar fr.inria.sacha.spoon.diffSpoon.DiffSpoonImpl $1 $2 $3
 
 	echo "#STATS_END"
 fi
