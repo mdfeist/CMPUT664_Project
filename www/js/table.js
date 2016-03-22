c36ed2eb18324e191699829f8179357870d88349
Eddie Antonio Santos <easantos@ualberta.ca>
2016-03-22 14:59:56 -0600

    Commit what we got.

---
+++ www/js/table.js
@@ -0,0 +1,26 @@
+window.DATA = {
+    "types": [
+        "list", "type", "names", "e.g.", "MyClass", "HashMap"
+    ],
+
+    "dates": [
+        {
+            "date": "2016-03-20 21:59:54 -0600",
+            "type": "MyClass",
+            "edit": "ADD",
+            "author": "name@example.com"
+        },
+        {
+            "date": "2016-03-20 21:59:54 -0600",
+            "type": "MyClass",
+            "edit": "REMOVE",
+            "author": "name@example.com"
+        },
+        {
+            "date": "2016-03-20 21:59:54 -0600",
+            "type": "HashMap",
+            "edit": "ADD",
+            "author": "name@example.com"
+        }
+    ]
+};
