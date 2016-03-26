2b5bfaa90e968771ff3a7ff56e668b308711ec12
Eddie Antonio Santos <easantos@ualberta.ca>
2016-03-26 16:30:09 -0600

    Add table from real data.

---
+++ www/js/table_example.js
@@ -0,0 +1,168 @@
+/*
+ * Based on the commit log of:
+ * https://github.com/Nightonke/BoomMenu
+ */
+window.DATA = {
+    "types": [
+      "com.nightonke.boommenu.Bar",
+      "com.nightonke.boommenu.BoomMenuButton",
+      "com.nightonke.boommenu.CircleButton",
+      "com.nightonke.boommenu.HamButton",
+      "com.nightonke.boommenusample.MainActivity",
+      "com.nightonke.boommenu.PlaceType",
+      "com.nightonke.boommenu.Util"
+    ],
+
+    "commits": [
+        {
+            "author": "nightonke@users.github.com",
+            "commitID": "efdac60d1931fe20ed8a38d6d868c68f8827c408",
+            "date": "2016-03-24T06:04:07Z",
+            "message": "Fix the bug when BMB is used in version below lollipop, it will crash."
+        },
+        {
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7",
+            "date": "2016-03-21T18:02:01Z",
+            "message": "Finish hamburger button."
+        }
+    ],
+
+    "dates": [
+        {
+            "date": "2016-03-24T06:04:07Z",
+            "type": "com.nightonke.boommenu.BoomMenuButton",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "efdac60d1931fe20ed8a38d6d868c68f8827c408"
+        },
+        {
+            "date": "2016-03-24T06:04:07Z",
+            "type": "com.nightonke.boommenu.BoomMenuButton",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "efdac60d1931fe20ed8a38d6d868c68f8827c408"
+        },
+
+        {
+            "date": "2016-03-24T06:04:07Z",
+            "type": "com.nightonke.boommenu.CircleButton",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "efdac60d1931fe20ed8a38d6d868c68f8827c408"
+        },
+        {
+            "date": "2016-03-24T06:04:07Z",
+            "type": "com.nightonke.boommenu.CircleButton",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "efdac60d1931fe20ed8a38d6d868c68f8827c408"
+        },
+
+        {
+            "date": "2016-03-24T06:04:07Z",
+            "type": "com.nightonke.boommenu.HamButton",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "efdac60d1931fe20ed8a38d6d868c68f8827c408"
+        },
+        {
+            "date": "2016-03-24T06:04:07Z",
+            "type": "com.nightonke.boommenu.HamButton",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "efdac60d1931fe20ed8a38d6d868c68f8827c408"
+        },
+
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenusample.MainActivity",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenusample.MainActivity",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.Bar",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.BoomMenuButton",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.BoomMenuButton",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.CircleButton",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.CircleButton",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.HamButton",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.PlaceType",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.PlaceType",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.Util",
+            "edit": "ADD",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        },
+        {
+            "date": "2016-03-21T18:02:01Z",
+            "type": "com.nightonke.boommenu.Util",
+            "edit": "REMOVE",
+            "author": "nightonke@users.github.com",
+            "commitID": "303d08e2fbb0d64ccbc64c98724cf56e5b31f9f7"
+        }
+    ]
+};
