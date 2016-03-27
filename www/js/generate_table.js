343c9548da6fa6f9af83ecb76264e0325627efa9
Eddie Antonio Santos <easantos@ualberta.ca>
2016-03-26 23:24:07 -0600

    Difference makes a bit more sense now...

--- www/js/generate_table.js
+++ www/js/generate_table.js
@@ -7,20 +7,28 @@
  *  - Ian Watts
  */
 
+// http://colorbrewer2.org/?type=diverging&scheme=RdBu&n=3
+// ['#ef8a62','#f7f7f7','#67a9cf'];
+var COLOR_START = '#ef8a62';
+var COLOR_END = '#67a9cf';
+
 /* Run right after the page (and this script) finishes loading. */
 document.addEventListener('DOMContentLoaded', function () {
-  /* Hack! */
+  var container = document.getElementById('dna-table');
+
+  /* XXX: */
   var processed = window.preprocessedData = preprocessData(window.DATA);
-  var filtered = window.filteredTypes = filterTypes(processed, {
-    //start: new Date('2016-03-19'),
-    //end: new Date('2016-03-21 0:00:00 -0600')
+  var filtered = window.filteredData = filterTypes(processed, {
   });
+
+  /* Make this arbitrarily large. */
+  var height = 20 * filtered.types.length;
+
+  drawGraph(filtered, container.offsetWidth, height);
 });
 
 /* Shim the assert function in there! */
 !window.assert ? (window.assert = console.assert.bind(console)) : undefined;
-
-// <data | preprocess data | filterTypes | drawGraph
 
 var VALID_STEP_SIZES = d3.set(['hour', 'day', 'month', 'week']);
 
@@ -132,6 +140,15 @@ Object.defineProperties(Cell.prototype, {
 
       return this;
     },
+  },
+
+  /**
+   * if the cell has demonstrable data.
+   */
+  hasData: {
+    get: function () {
+      return this.data && (this.data.length > 0);
+    }
   }
 });
 
@@ -211,27 +228,37 @@ function filterTypes(data, filters) {
     })
     .slice(0, numberOfTypesUpperBound);
 
-  var types = sortedTypeNames.map(function (name) {
-    return {
-      name: name,
-      cells: []
-    };
-  });
+  assert(sortedTypeNames.length > 0);
+
+  var cells = [];
+  var numberOfColumns = 0;
 
   /**
    * Create all cells applicable to display.
    */
   forEachDateLimitsDescending(startDate, endDate, cellSize, function (start, end) {
     /* For each type... */
-    types.forEach(function (type) {
+    sortedTypeNames.forEach(function (typeName) {
       /* Add all the cells that it covers. */
-      type.cells.push(new Cell(start, end, type.name)
-                      .setDataFromSortedASTDiffs(applicableDiffs)
+      cells.push(new Cell(start, end, typeName)
+                 .setDataFromSortedASTDiffs(applicableDiffs)
       );
     });
+
+    numberOfColumns++;
   });
 
-  return types;
+  var maxDate = cells[0].endDate;
+  var minDate = cells[cells.length - 1].startDate;
+  assert(minDate <= maxDate);
+
+  return {
+    types: sortedTypeNames,
+    cells: cells,
+    numberOfColumns: numberOfColumns,
+    minDate: minDate,
+    maxDate: maxDate
+  };
 }
 
 
@@ -280,7 +307,70 @@ function forEachDateLimitsDescending(start, end, step, callback) {
   }
 }
 
+/*=== Graph ===*/
 
+function drawGraph(data, width, height) {
+  //var cellWidth = width / data.numberOfColumns;
+
+  /* Create a scale for the types i.e., the y-axis */
+  var yScale = d3.scale.ordinal()
+    .domain(data.types)
+    .rangeBands([0, height]);
+
+  /* Create a scale for the dates i.e., the x-axis */
+  var xScale = d3.time.scale()
+    .domain([data.minDate, data.maxDate])
+    .rangeRound([0, width]);
+
+  /**
+   * Interpolates colours
+   */
+  var colorInterpolator = d3.interpolateHsl(COLOR_START, COLOR_END);
+  var colorScale = function (_additions, _deletions) {
+    var score = 0;
+    assert(0 <= score && score <= 1);
+    return colorInterpolator(score);
+  };
+
+  var svg = d3.select('#dna-table').append('svg')
+      .attr("width", width)
+      .attr("height", height);
+
+  /* Create all the cells. */
+  var cells = svg.selectAll('.cell')
+      .data(data.cells)
+    .enter().append('g')
+      .attr('class', 'cell')
+      .attr('transform', function (cell) {
+        return 'translate(' +
+          xScale(cell.startDate) +
+          ',' +
+          yScale(cell.type) +
+          ')';
+      });
+
+  cells.append('rect')
+    .attr('class', 'cell-data')
+    .attr('width', cellWidthFromScale(data.cells[0], xScale))
+    .attr('height', yScale.rangeBand())
+    .style('fill', function (cell) {
+      if (cell.hasData) {
+        return colorScale(0, 0);
+      } else {
+        return 'none';
+      }
+    })
+}
+
+/*=== Utilties ===*/
+
+function cellWidthFromScale(cell, scale) {
+  var bigger = scale(cell.endDate);
+  var smaller = scale(cell.startDate);
+  assert(bigger > smaller);
+  return bigger - smaller;
+
+}
 
 /*=== Predicates used in assertions and checks ===*/
 
@@ -303,198 +393,4 @@ function looksLikeAGitSha(thing) {
 }
 
 
-/*=== The old implementation ===*/
-
-function old() {
-  /*
-   * Derived from:
-   * https://bost.ocks.org/mike/misrables/
-   */
-  var margin = {top: 80, right: 0, bottom: 10, left: 80},
-      width = 720,
-      height = 720;
-
-  var x = d3.scale.ordinal().rangeBands([0, width]),
-      y = d3.scale.ordinal().rangeBands([0, height]);
-
-  var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .style("margin-left", -margin.left + "px")
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  function Bin(start, end) {
-    console.assert(start < end);
-    this.start = start;
-    this.end = end;
-
-    this.types = [];
-  }
-
-  Bin.prototype.check = function (dateObj) {
-    var date = new Date(dateObj.date)
-    return this.start <= date && date < this.end;
-  };
-
-  Bin.prototype.add = function (dateObj) {
-    console.assert(this.check(dateObj));
-    this.types.push(dateObj);
-  };
-
-
-  function makeBins(start, end, step) {
-    /* "Round down" to a decent value. */
-    var currentStart = start;
-    var currentEnd;
-    var bins = [];
-
-    while ((currentEnd === undefined) ||  currentEnd < end) {
-      currentEnd = moment(new Date(currentStart));
-
-      switch (step) {
-          case 'hour':
-              currentEnd.add(1, 'hour');
-              break;
-          case 'day':
-              currentEnd.add(1, 'day');
-              break;
-          case 'week':
-              /* Make smarter? */
-              currentEnd.add(1, 'weeks');
-              break;
-          case 'month':
-              currentEnd.add(1, 'month');
-      }
-
-      bins.push(new Bin(currentStart, currentEnd.toDate()));
-      currentStart = currentEnd.toDate();
-    }
-
-    return bins;
-  }
-
-
-  function renderGraph(table, options) {
-    var rows = table.types.length;
-
-    /* TODO: Make bins somehow. */
-    /*
-     * TODO:
-     *  - Calculate how many bins based on date range, step size.
-     *  - Create empty bins
-     *  - Add types to bins (based on dates)
-     */
-    var bins = makeBins(options.from, options.to, options.stepSize);
-    var columns = bins.length;
-
-    table.dates.forEach(function (item) {
-      bins.forEach(function (bin) {
-        if (bin.check(item)) {
-          bin.add(item);
-          return true;
-        }
-      });
-    });
-
-    /* Create a count of all the types actually binnned, plus their absolute frequency.  */
-    var typesFrequency = {};
-    bins.forEach(function (bin) {
-      bin.types.forEach(function (item) {
-        var freq = typesFrequency[item.type] || 0;
-        typesFrequency[item.type] = freq + 1;
-      });
-    });
-
-    var tuples = Object.keys(typesFrequency).map(function (typeName) {
-      return { name: typeName, frequency: typesFrequency[typeName] };
-    });
-
-    /* Sort types on FREQUENCY USED, (such that it can be used while filtering).  */
-    tuples.sort(function (a, b) {
-      return a.frequency < b.frequency ? 1 : (a.frequency > b.frequency ? -1 : 0);
-    });
-
-    if (options.filter !== Infinity) {
-      tuples = tuples.slice(options.filter);
-
-      /* Set of types used. */
-      var typesUsed = {};
-      tuples.forEach(function (item) {
-        typesUsed[item.name] = 1;
-      });
-
-      rows = tuples.length;
-      /* Remove types from bins based off filtering. */
-      bins.forEach(function (bin) {
-        bin.types = bin.types.filter(function (item) {
-          return !(item.type in typesUsed);
-        });
-      });
-    }
-
-    x.domain(d3.range(columns));
-    y.domain(d3.range(rows));
-
-    /* Outer rectangle. */
-    svg.append("rect")
-        .attr("class", "background")
-        .attr("width", width)
-        .attr("height", height);
-
-    var row = svg.selectAll(".row")
-        .data(tuples)
-      .enter().append("g")
-        .attr("class", "row")
-        .attr("transform", function(_d, i) { return "translate(0," + y(i) + ")"; })
-        .each(row_fn);
-
-    row.append("line")
-        .attr("x2", width);
-
-    row.append("text")
-        .attr("x", -6)
-        .attr("y", y.rangeBand() / 2)
-        .attr("dy", ".32em")
-        .attr("text-anchor", "end")
-        .text(function(d, i) { return d.name; });
-
-    var column = svg.selectAll(".column")
-        .data(bins)
-      .enter().append("g")
-        .attr("class", "column");
-
-    column.append("line")
-        .attr("x1", -width);
-
-    column.append("text")
-        .attr("x", 6)
-        .attr("y", x.rangeBand() / 2)
-        .attr("dy", ".32em")
-        .attr("text-anchor", "start")
-        .attr("transform", function(_d, i) { return "translate(" + x(i) + ")rotate(90)"; })
-        .text(function(bin, i) { return bin.start; });
-
-    function row_fn(row) {
-      var cell = d3.select(this).selectAll(".cell")
-          .data(bins)
-        .enter().append("rect")
-          .attr("class", "cell")
-          .attr("x", function(d) { return x(d.x); })
-          .attr("width", x.rangeBand())
-          .attr("height", y.rangeBand())
-          .style("fill", function(d) { return '#ff00ff'; })
-    }
-  }
-
-  /* Just render the data! */
-  renderGraph(window.DATA, {
-    from: new Date('2016-03-20 19:59:54 -0600'),
-    to: new Date('2016-03-21 21:58:54 -0600'),
-    stepSize: 'day',
-    limit: Infinity
-  });
-}
-
-/*globals d3*/
-/*eslint no-use-before-define: true */
+/*globals d3, moment*/
