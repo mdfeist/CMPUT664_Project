d9ec790425843da1f5cc43de74dc063fa15dc0d6
Michael Feist <mdfeist@users.noreply.github.com>
2016-04-07 12:13:02 -0600

    Delete TestJava.java

--- TestJava.java
+++
@@ -1,9 +0,0 @@
-public class Main extends AbstractTree implements ITree {
-	String string = null;
-	List<Integer> ints = new ArrayList<Integer>();
-	Map<String,Integer> map = new Map<String,Integer>();
-
-	void foo() {
-		int m = 0;
-	}
-};
