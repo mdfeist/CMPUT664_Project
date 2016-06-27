22d491743cb0545037e9b7a076c4d2ac6090de23
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-27 15:30:22 -0600

    Start to refactor timeline view into its own class.

--- www/js/project-view.ts
+++ www/js/project-view.ts
@@ -77,283 +77,302 @@ export function createTable2(filter: Filter) {
 
 /*=== Graph ===*/
 
-function drawGraph(data: DataView, width: number) {
-  var marginLeft = 150;
-  var cellHeight = 64;
-  var height = cellHeight * data.types.length;
+const Timeline = class Timeline {
+  public marginLeft = 150;
+  public cellHeight = 64;
+  public height: number;
 
-  /* XXX: Sneak in a little info about the project. */
-  let summaryText = d3.select('body').selectAll('.summary')
-      .data([summary(data)])
-      .text(d => d)
-    .enter().append('pre')
-      .classed('summary', true)
-      .text(d => d)
-      .style('display', 'none');
-
-  /* Create a scale for the types i.e., the y-axis */
-  var yScale = d3.scale.ordinal()
-    .domain(data.types.map(type => type.fullyQualifiedName ))
-    .rangeBands([0, height]);
-
-  /* Create a scale for the dates i.e., the x-axis */
-  var xScale = d3.time.scale()
-    .domain([data.minDate, data.maxDate])
-    .range([marginLeft, width]);
-
-  var timeAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient('bottom');
-
-  //var cellWidth = cellWidthFromScale(first(first(data.types).cells), xScale);
-  var maxCellHeight = yScale.rangeBand() - 2;
-
-  var svg = d3.select('#dna-table').append('svg')
-      .classed('main-figure', true)
-      .attr("width", width)
-      .attr("height", height);
-
-  var row = svg.selectAll('.row')
-      .data(data.types)
-    .enter().append('g')
-      .classed('row', true)
-      .attr('transform', (type) => `translate(0, ${yScale(type.fullyQualifiedName)})`);
-
-  /* The background. */
-  row.append('rect')
-      .attr('x', marginLeft)
-      .attr('width', width - marginLeft)
-      .attr('height', maxCellHeight)
-      .style('fill', function (_, i) {
-        /* Make alternating colour bands. */
-        return i & 1 ? '#f4f4f4' : '#fafafa';
-      });
-
-  /* Create a closure over data.commits. Function#bind() won't work
-   * since d3 wants to set up its own `this` to the callback! */
-  let createCell = function (type: JavaType) {
-    return createCellsForType.call(this, data.commits, type);
-  }
-  row.each(createCell);
-
-  row.append('text')
-      .classed('type-title', true)
-      .attr('y', yScale.rangeBand() / 2)
-      .attr('dy', '.22em')
-      .attr('x', `${marginLeft - 10}px`)
-      .attr('text-anchor', 'end')
-      .text((type) => type.shortName );
-
-  /* Add the time axis as a **new** SVG element, inserted BEFORE the main SVG
-   * element.*/
-  var floatingAxis = d3.select('#dna-table').insert('svg', '.main-figure')
-      .classed('time-axis', true)
-      .attr('width', width)
-      /* Note: Height will be set with CSS. */
-      .call(timeAxis);
-
-  /* Make all text left-aligned, and bold all the years. */
-  floatingAxis.selectAll("text")
-      .attr("y", 6)
-      .attr("x", 6)
-      .style("text-anchor", "start")
-      .classed('year-tick', function () {
-        /* Matches if the text looks like a year. */
-        var text = this.textContent;
-        return text.match(/^\d{4,}$/);
-      });
-
-  ensureAxisIsAtGraphBottom(
-    asSVGElement(svg.node()),
-    asSVGElement(floatingAxis.node())
-  );
-
-  /* Dynamic type assertion for SVGElement. */
-  function asSVGElement(value: any): SVGElement {
-    if (value instanceof SVGElement) {
-      return value;
-    }
-    throw new TypeError('value is not an SVGElement.');
+  constructor(private data: DataView, public width: number) {
+    this.height = this.cellHeight * data.types.length;
   }
 
-  function createCellsForType(commits: CommitMap, type: JavaType) {
-    /* Create all the cells. */
-    var cell = d3.select(this).selectAll('.cell')
-      .data(type.cells)
+  draw() {
+    this.insertLatexSummary();
+
+    const {width, height, data, marginLeft} = this;
+
+    /* Create a scale for the types i.e., the y-axis */
+    var yScale = d3.scale.ordinal()
+      .domain(data.types.map(type => type.fullyQualifiedName ))
+      .rangeBands([0, height]);
+
+    /* Create a scale for the dates i.e., the x-axis */
+    var xScale = d3.time.scale()
+      .domain([data.minDate, data.maxDate])
+      .range([marginLeft, width]);
+
+    var timeAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient('bottom');
+
+    var maxCellHeight = yScale.rangeBand() - 2;
+
+    var svg = d3.select('#dna-table').append('svg')
+        .classed('main-figure', true)
+        .attr("width", width)
+        .attr("height", height);
+
+    var row = svg.selectAll('.row')
+        .data(data.types)
       .enter().append('g')
-        /* Only do cool things with cells that *HAVE* data! */
-        .filter(function (cell: Cell) { return cell.hasData; })
-        .classed('cell', true)
-        .attr('transform', function (cell) {
-          var yOffset = maxCellHeight * (1  - cell.numberOfObservations / type.numberOfObservationsInLargestCell);
-          return 'translate(' + xScale(cell.startDate) + ', ' + yOffset + ')';
+        .classed('row', true)
+        .attr('transform', (type) => `translate(0, ${yScale(type.fullyQualifiedName)})`);
+
+    /* The background. */
+    row.append('rect')
+        .attr('x', marginLeft)
+        .attr('width', width - marginLeft)
+        .attr('height', maxCellHeight)
+        .style('fill', function (_, i) {
+          /* Make alternating colour bands. */
+          return i & 1 ? '#f4f4f4' : '#fafafa';
         });
 
-    /* Make the deletion bar. */
-    cell.append('rect')
-      .classed('ast-deletions', true)
-      .attr('width', function (cell) {
-        return cellWidthFromScale(cell, xScale);
-      })
-      .attr('height', function (cell) {
-        var proportion = cell.numberOfDeletions / cell.numberOfObservations;
-        var height = cell.numberOfObservations / type.numberOfObservationsInLargestCell;
-        return proportion * maxCellHeight * height;
-      });
+    /* Create a closure over data.commits. Function#bind() won't work
+     * since d3 wants to set up its own `this` to the callback! */
+    let createCell = function (type: JavaType) {
+      return createCellsForType.call(this, data.commits, type);
+    }
+    row.each(createCell);
 
-    /* Make the addition bar. */
-    cell.append('rect')
-      .classed('ast-additions', true)
-      .attr('width', function (cell) {
-        return cellWidthFromScale(cell, xScale);
-      })
-      .attr('transform', function (cell) {
-        var proportion = cell.numberOfDeletions / cell.numberOfObservations;
-        var height = cell.numberOfObservations / type.numberOfObservationsInLargestCell;
-        var topHalf = proportion * maxCellHeight * height;
+    row.append('text')
+        .classed('type-title', true)
+        .attr('y', yScale.rangeBand() / 2)
+        .attr('dy', '.22em')
+        .attr('x', `${marginLeft - 10}px`)
+        .attr('text-anchor', 'end')
+        .text((type) => type.shortName );
 
-        return `translate(0, ${topHalf})`;
-      })
-      .attr('height', function (cell) {
-        var proportion = cell.numberOfAdds / cell.numberOfObservations;
-        var height = cell.numberOfObservations / type.numberOfObservationsInLargestCell;
-        return proportion * maxCellHeight * height;
-      });
+    /* Add the time axis as a **new** SVG element, inserted BEFORE the main SVG
+     * element.*/
+    var floatingAxis = d3.select('#dna-table').insert('svg', '.main-figure')
+        .classed('time-axis', true)
+        .attr('width', width)
+        /* Note: Height will be set with CSS. */
+        .call(timeAxis);
 
-    /* Mouse Click: Show Cell stats */
-    cell.on("click", function(cellData) {
-      var stats = d3.select("#stats-body");
-      stats.selectAll('.content').remove();
+    /* Make all text left-aligned, and bold all the years. */
+    floatingAxis.selectAll("text")
+        .attr("y", 6)
+        .attr("x", 6)
+        .style("text-anchor", "start")
+        .classed('year-tick', function () {
+          /* Matches if the text looks like a year. */
+          var text = this.textContent;
+          return text.match(/^\d{4,}$/);
+        });
 
-      var info = stats.append('div')
-        .classed('content', true);
+    ensureAxisIsAtGraphBottom(
+      asSVGElement(svg.node()),
+      asSVGElement(floatingAxis.node())
+    );
 
-      info.append('b')
-        .text("Info:");
-      info.append('p')
-        .text("Additions: " + cellData.numberOfAdds);
-      info.append('p')
-        .text("Deletions: " + cellData.numberOfDeletions);
-      /* TODO: Graphs here! */
-      info.append('p')
-        .text("Authors: " + cellData.authors.length);
-      info.append('p')
-        .text("Commits: " + cellData.commits.length);
+    /* Dynamic type assertion for SVGElement. */
+    function asSVGElement(value: any): SVGElement {
+      if (value instanceof SVGElement) {
+        return value;
+      }
+      throw new TypeError('value is not an SVGElement.');
+    }
 
-      info.append('br');
+    function createCellsForType(commits: CommitMap, type: JavaType) {
+      /* Create all the cells. */
+      var cell = d3.select(this).selectAll('.cell')
+        .data(type.cells)
+        .enter().append('g')
+          /* Only do cool things with cells that *HAVE* data! */
+          .filter(function (cell: Cell) { return cell.hasData; })
+          .classed('cell', true)
+          .attr('transform', function (cell) {
+            var yOffset = maxCellHeight * (1  - cell.numberOfObservations / type.numberOfObservationsInLargestCell);
+            return 'translate(' + xScale(cell.startDate) + ', ' + yOffset + ')';
+          });
 
-      info.append('b')
-        .text("Authors:");
+      /* Make the deletion bar. */
+      cell.append('rect')
+        .classed('ast-deletions', true)
+        .attr('width', function (cell) {
+          return cellWidthFromScale(cell, xScale);
+        })
+        .attr('height', function (cell) {
+          var proportion = cell.numberOfDeletions / cell.numberOfObservations;
+          var height = cell.numberOfObservations / type.numberOfObservationsInLargestCell;
+          return proportion * maxCellHeight * height;
+        });
 
-      cellData.authors.forEach(function (author) {
+      /* Make the addition bar. */
+      cell.append('rect')
+        .classed('ast-additions', true)
+        .attr('width', function (cell) {
+          return cellWidthFromScale(cell, xScale);
+        })
+        .attr('transform', function (cell) {
+          var proportion = cell.numberOfDeletions / cell.numberOfObservations;
+          var height = cell.numberOfObservations / type.numberOfObservationsInLargestCell;
+          var topHalf = proportion * maxCellHeight * height;
+
+          return `translate(0, ${topHalf})`;
+        })
+        .attr('height', function (cell) {
+          var proportion = cell.numberOfAdds / cell.numberOfObservations;
+          var height = cell.numberOfObservations / type.numberOfObservationsInLargestCell;
+          return proportion * maxCellHeight * height;
+        });
+
+      /* Mouse Click: Show Cell stats */
+      cell.on("click", function(cellData) {
+        var stats = d3.select("#stats-body");
+        stats.selectAll('.content').remove();
+
+        var info = stats.append('div')
+          .classed('content', true);
+
+        info.append('b')
+          .text("Info:");
         info.append('p')
-          .text(author);
-      });
-
-      info.append('br');
-
-      info.append('b')
-        .text("Commits:");
-
-      cellData.commits.forEach(function (commitID) {
-        var commit = commits[commitID];
+          .text("Additions: " + cellData.numberOfAdds);
         info.append('p')
-          .text("Commit ID: " + commit.commitID);
-        var block = info.append('div')
-          .style('padding-left', '5em');
-        block.append('p')
-          .text("Author: " + commit.author);
-        block.append('p')
-          .text("Date: " + commit.date);
-        block.append('p')
-          .text("Message: ");
+          .text("Deletions: " + cellData.numberOfDeletions);
+        /* TODO: Graphs here! */
+        info.append('p')
+          .text("Authors: " + cellData.authors.length);
+        info.append('p')
+          .text("Commits: " + cellData.commits.length);
 
-        var msg_block = block.append('div')
-          .style('padding-left', '5em');
+        info.append('br');
 
-        var lines = commit.message.split("\n");
-        lines.forEach(function (line) {
-          msg_block.append('p')
-          .text(line);
+        info.append('b')
+          .text("Authors:");
+
+        cellData.authors.forEach(function (author) {
+          info.append('p')
+            .text(author);
         });
 
         info.append('br');
+
+        info.append('b')
+          .text("Commits:");
+
+        cellData.commits.forEach(function (commitID) {
+          var commit = commits[commitID];
+          info.append('p')
+            .text("Commit ID: " + commit.commitID);
+          var block = info.append('div')
+            .style('padding-left', '5em');
+          block.append('p')
+            .text("Author: " + commit.author);
+          block.append('p')
+            .text("Date: " + commit.date);
+          block.append('p')
+            .text("Message: ");
+
+          var msg_block = block.append('div')
+            .style('padding-left', '5em');
+
+          var lines = commit.message.split("\n");
+          lines.forEach(function (line) {
+            msg_block.append('p')
+            .text(line);
+          });
+
+          info.append('br');
+        });
       });
-    });
 
-    /* Mouse over: Show and update cell info */
-    cell.on("mouseover", function(cell_data: Cell) {
-      cellInfo.selectAll('ul').remove();
-      var info = cellInfo.append('ul')
-        .classed('list-group', true);
+      /* Mouse over: Show and update cell info */
+      cell.on("mouseover", function(cell_data: Cell) {
+        cellInfo.selectAll('ul').remove();
+        var info = cellInfo.append('ul')
+          .classed('list-group', true);
 
-      info.append('li')
-        .classed("list-group-item", true)
-        .text("Type: " + cell_data.type);
+        info.append('li')
+          .classed("list-group-item", true)
+          .text("Type: " + cell_data.type);
 
-      info.append('li')
-        .classed("list-group-item", true)
-        .text("Additions: " + cell_data.numberOfAdds);
+        info.append('li')
+          .classed("list-group-item", true)
+          .text("Additions: " + cell_data.numberOfAdds);
 
-      info.append('li')
-        .classed("list-group-item", true)
-        .text("Deletions: " + cell_data.numberOfDeletions);
+        info.append('li')
+          .classed("list-group-item", true)
+          .text("Deletions: " + cell_data.numberOfDeletions);
 
-      info.append('li')
-        .classed("list-group-item", true)
-        .text("Authors: " + cell_data.authors.length);
+        info.append('li')
+          .classed("list-group-item", true)
+          .text("Authors: " + cell_data.authors.length);
 
-      info.append('li')
-        .classed("list-group-item", true)
-        .text("Commits: " + cell_data.commits.length);
+        info.append('li')
+          .classed("list-group-item", true)
+          .text("Commits: " + cell_data.commits.length);
 
-      info.append('li')
-        .classed("list-group-item", true)
-        .text("Start Date: " + cell_data.startDate);
-      info.append('li')
-        .classed("list-group-item", true)
-        .text("End Date: " + cell_data.endDate);
+        info.append('li')
+          .classed("list-group-item", true)
+          .text("Start Date: " + cell_data.startDate);
+        info.append('li')
+          .classed("list-group-item", true)
+          .text("End Date: " + cell_data.endDate);
 
-      var coords = d3.mouse(document.body);
-      var currentx = coords[0];
-      var currenty = coords[1];
+        var coords = d3.mouse(document.body);
+        var currentx = coords[0];
+        var currenty = coords[1];
 
-      var x = currentx - CELL_INFO_WIDTH/2;
+        var x = currentx - CELL_INFO_WIDTH/2;
 
-      if (x < 10) x = 10;
-      if (x > width - CELL_INFO_WIDTH - 10) x = width - CELL_INFO_WIDTH -10;
+        if (x < 10) x = 10;
+        if (x > width - CELL_INFO_WIDTH - 10) x = width - CELL_INFO_WIDTH -10;
 
-      var y = currenty + maxCellHeight + 10;
+        var y = currenty + maxCellHeight + 10;
 
-      cellInfo.style('left', String(x) + "px");
-      cellInfo.style('top', String(y) + "px");
+        cellInfo.style('left', String(x) + "px");
+        cellInfo.style('top', String(y) + "px");
 
-      cellInfo.style("visibility", "visible");
-    });
+        cellInfo.style("visibility", "visible");
+      });
 
-    /* Mouse move: Update position of cell info */
-    cell.on("mousemove", function (_cell_data) {
-      var coords = d3.mouse(document.body);
-      var currentx = coords[0];
-      var currenty = coords[1];
+      /* Mouse move: Update position of cell info */
+      cell.on("mousemove", function (_cell_data) {
+        var coords = d3.mouse(document.body);
+        var currentx = coords[0];
+        var currenty = coords[1];
 
-      var x = currentx - CELL_INFO_WIDTH/2;
+        var x = currentx - CELL_INFO_WIDTH/2;
 
-      if (x < 10) x = 10;
-      if (x > width - CELL_INFO_WIDTH - 10) x = width - CELL_INFO_WIDTH -10;
+        if (x < 10) x = 10;
+        if (x > width - CELL_INFO_WIDTH - 10) x = width - CELL_INFO_WIDTH -10;
 
-      var y = currenty + maxCellHeight + 10;
+        var y = currenty + maxCellHeight + 10;
 
-      cellInfo.style('left', String(x) + "px");
-      cellInfo.style('top', String(y) + "px");
-    });
+        cellInfo.style('left', String(x) + "px");
+        cellInfo.style('top', String(y) + "px");
+      });
 
-    /* Mouse out: Hide cell info */
-    cell.on("mouseout", function(_cell) {
-      cellInfo.style("visibility", "hidden");
-    });
+      /* Mouse out: Hide cell info */
+      cell.on("mouseout", function(_cell) {
+        cellInfo.style("visibility", "hidden");
+      });
+    }
+  }
+
+  /**
+   * Sneak in a little info about the project.
+   */
+  insertLatexSummary() {
+    d3.select('body').selectAll('.summary')
+        .data([summary(this.data)])
+        .text(d => d)
+      .enter().append('pre')
+        .classed('summary', true)
+        .text(d => d)
+        .style('display', 'none');
   }
 }
+
+
+function drawGraph(data: DataView, width: number) {
+  new Timeline(data, width).draw();
+}
+
 
 /**
  * Draws type coverage stats and things.
